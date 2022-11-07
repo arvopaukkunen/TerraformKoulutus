bc7343e9910583153a4577cea86ec104bc5f6dc8
Arvo Paukkunen <arvosoft@hotmail.com>
2022-11-07 15:16:59 +0200

    vtt base vnet 1 ok

--- 1-main-vnet/main.tf
+++ 1-main-vnet/main.tf
@@ -27,12 +27,12 @@ variable "location" {
 
 variable "vnet_cidr_range" {
   type    = string
-  default = "10.10.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "subnet_prefixes" {
   type    = list(string)
-  default = ["10.10.0.0/24", "10.0.1.0/24"]
+  default = ["10.0.0.0/24", "10.0.1.0/24"]
 }
 
 variable "subnet_names" {
